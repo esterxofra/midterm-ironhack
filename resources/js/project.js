d4ef2d9c429e53ba36853190d8fd1b49a820599b
Ester Xofra <esterxofra@gmail.com>
2023-01-26 18:24:51 +0100

    responsive Jarko

--- resources/js/project.js
+++ resources/js/project.js
@@ -1,2 +1,18 @@
 /* Crea tu propia lógica para hacer fetch de un post y enseñar su información utilizando DOM manipulation */
 /* ADVANCED: consigue que la info del post funcione dinámicamente y enseñe un post u otro según la URL */
+
+const hamburger = document.querySelector(".hamburger");
+
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
