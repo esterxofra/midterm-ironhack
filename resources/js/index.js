937aa6ae168ba2a8897866374a6da0d9740cf299
Ester Xofra <esterxofra@gmail.com>
2023-01-26 12:14:19 +0100

    hamburger menu

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,15 @@
 /* Crea tu propia lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation */
+
+const hamburger = document.querySelector(".hamburger");
+
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
