7193632ae853a70d7b7956406635fed48249072e
Ester Xofra <esterxofra@gmail.com>
2023-01-26 17:39:54 +0100

    responsive

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -5,47 +5,50 @@
 
 let form = document.querySelector("#form");
 
-let addPost =(e) => {
-    // Verificacion y "refrescar" conceptos de semana pasada
-    e.preventDefault();
-    console.log(e);
-    console.log("CTA Connected");
-    // Vamos a traernos el valor de los inputs del html
-    // let titleOfUserInput = document.querySelector("#title").value;
-    // let bodyOfUserInput = document.querySelector("#body").value;
-    //Los actuales
-    let nameOfInput = document.querySelector("#name").value;
-    let emailOfInput = document.querySelector("email").value;
-    let messageOfInput = document.querySelector("message").value;
-  
-    // Uso de fetch para empujar valores del title y el body del formulario al api PUBLICO externo
-    fetch("https://jsonplaceholder.typicode.com/", {
-      method: "POST",
-      body: JSON.stringify({
-        // SYNTAX DE REFERENCIA de pagina web
-        // https://jsonplaceholder.typicode.com/guide/
-        //   title: "me gusta comprar mucho los bocatas del mercadona",
-        //   body: "guilty pleasures",
-        //   userId: 1,
-  
-        // ------
-        // ------
-        // ------
-        // ------
-        // NUESTRO SYNTAX para conectar los inputs de texto + textArea con esta llamada API
-        //nombreDeClave CON la variable que guarda el input.value del dom selection
-        name: nameOfInput,
-        email: emailOfInput,
-        body: messageOfInput,
-      }),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-  };
-  
-  //Definir logica para conectar mediante DOM a elementos de bottones que usamos en clase:
-  //document.querySelector("#addPost").addEventListener("submit", addPost);
-  form.addEventListener('submit', addPost);
+let addPost = (e) => {
+  // Verificacion y "refrescar" conceptos de semana pasada
+  e.preventDefault();
+  console.log(e);
+  console.log("CTA Connected");
+  // Vamos a traernos el valor de los inputs del html
+  // let titleOfUserInput = document.querySelector("#title").value;
+  // let bodyOfUserInput = document.querySelector("#body").value;
+  //Los actuales
+  let nameOfInput = document.querySelector("#name").value;
+  let emailOfInput = document.querySelector("#email").value;
+  let messageOfInput = document.querySelector("#message").value;
+
+  // Uso de fetch para empujar valores del title y el body del formulario al api PUBLICO externo
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      // SYNTAX DE REFERENCIA de pagina web
+      // https://jsonplaceholder.typicode.com/guide/
+      //   title: "me gusta comprar mucho los bocatas del mercadona",
+      //   body: "guilty pleasures",
+      //   userId: 1,
+
+      // ------
+      // ------
+      // ------
+      // ------
+      // NUESTRO SYNTAX para conectar los inputs de texto + textArea con esta llamada API
+      //nombreDeClave CON la variable que guarda el input.value del dom selection
+      name: nameOfInput,
+      email: emailOfInput,
+      body: messageOfInput,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch((error) => error);
+  // usamos este nuevo metodo reset() para lmpiar los inputs :)
+  form.reset();
+};
+
+//Definir logica para conectar mediante DOM a elementos de bottones que usamos en clase:
+//document.querySelector("#addPost").addEventListener("submit", addPost);
+form.addEventListener("submit", addPost);
--- resources/js/index.js
+++ resources/js/index.js
@@ -5,11 +5,85 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+  })
+);
+
+// fetch(`https://jsonplaceholder.typicode.com/`, {
+//   method: "GET",
+//   headers: {
+//     "Content-Type": "application/json; charset=UTF-8",
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+
+// ADDING TITLES OF THE SECTION
+
+const titlesProjects = document.querySelectorAll(".internal-div-project h3");
+let getExternalTitle = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((respuestas) => {
+      console.log(respuestas[0].title);
+      let infoFromApi = "";
+      titlesProjects.forEach((header, index) => {
+        header.innerHTML = respuestas[index].title.substring(0, 10);
+      });
+      return (document.querySelector("#output").innerHTML = infoFromApi);
+    })
+    .catch((error) => console.log(error));
+};
+getExternalTitle();
+
+// ADDING TITLES OF THE SECTION
+//   const bodyProjects = document.querySelectorAll(".internal-div-project p");
+//   console.log(bodyProjects);
+//   let getExternalBody = () => {
+//     fetch("https://jsonplaceholder.typicode.com/posts")
+//       .then((response) => response.json())
+//       .then((respuestas) => {
+//         console.log(respuestas[0].body);
+//         let infoFromApiTwo = "";
+//         titlesProjects.forEach((content, index) => {
+//             content.innerHTML = respuestas[index].body;
+//             content.innerHTML = content.innerHTML.substring(0, 70);
+//         });
+//         return (document.querySelector("#output").innerHTML = infoFromApiTwo);
+//       })
+//       .catch((error) => console.log(error));
+//   };
+//   getExternalBody();
+
+const getPosts = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((respuesta) => {
+      for (let i = 0; i < 3; i++) {
+        console.log(respuesta[i]);
+        console.log(respuesta[i].title);
+        document.querySelector(".external-div-project").innerHTML += `
+        <div class="internal-div-project">
+          <img class="image-project" src="./resources/images/projects-section/${
+            respuesta[i].id
+          }.jpg" alt="">
+          <h3>${respuesta[i].title.slice(0, 10)}...</h3>
+          <p>${respuesta[i].body.slice(0, 50)}</p>
+          <button class="learn-more-button" type="button"><a href="project.html">Learn More</a></button>
+        </div>
+        `;
+      }
+    })
+    .catch();
+};
+
+//getPosts()
+window.addEventListener("load", getPosts);
